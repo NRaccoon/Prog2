d0014c77e433d2cb789968fc16553900282e4308
Ninjaraccoon <ninjaraccoon4@gmail.com>
2019-12-01 20:03:43 +0100

    new

--- Chomsky2/main.cpp
+++ Chomsky2/main.cpp
@@ -4,22 +4,26 @@
 
 using namespace std;
 
-int main(int argc, char ++argv){
-png::image <png::rgb_pixel> png_image(argv[1]);
+int main(int argc, char **argv)
+{
+	png::image <png::rgb_pixel> png_image(argv[1]);
 
-int size = png_image.get_width()*png_image.get_height();
+	int size = png_image.get_width()*png_image.get_height();
 
-Perceptron *p = new Perceptron(3, size, 256, 1);
+	Perceptron *p = new Perceptron(3, size, 256, 1); //3 a rétegek száma, ezt követő 3 érték a rétegenkénti neuronok száma
 
-double* image = new double[size];
-for(int i = 0; i<png_inamge.get_width(); ++i)
-	for(int j = 0; i<png_image.get_height(); ++j)
-	image[i*png_image.get_width()+j] = png_image[i][j].red;
+	double* image = new double[size];
 
-double value = (*p) (image);
+	for(int i=0; i<png_image.get_width(); ++i)
+		for(int j=0; j<png_image.get_height(); ++j)
+			image[i*png_image.get_width()+j] = png_image[i][j].red;
 
-cout << value << endl;
 
-delete p;
-delete [] image;
+	double value = (*p)(image);
+
+	cout<<value;
+
+	delete p;
+	delete [] image;
 }
+
